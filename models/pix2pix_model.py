580f4e287873984c0791b6ef89f60e5d14a88919
Jerry Lingjie Mei <jerrymei@Jerrys-MacBook-Pro.local>
2018-12-09 23:12:38 -0500

    Switch to 128

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -13,7 +13,7 @@ class Pix2PixModel(BaseModel):
 
         # changing the default values to match the pix2pix paper
         # (https://phillipi.github.io/pix2pix/)
-        parser.set_defaults(norm='batch', netG='unet_256')
+        parser.set_defaults(norm='batch', netG='unet_128')
         parser.set_defaults(dataset_mode='aligned')
         if is_train:
             parser.set_defaults(pool_size=0, no_lsgan=True)
